58347f3012fa33e3f807b56dbd4c95686c1279db
Ossi <101654124+ossi-hy@users.noreply.github.com>
2022-04-08 16:17:29 +0300

    extended solver test

--- src/tests/solver_test.py
+++ src/tests/solver_test.py
@@ -7,9 +7,21 @@ class TestSolver(unittest.TestCase):
         self.board = Board()
         self.solver = Solver()
     
-    def test_find_winning_one_move_ahead(self):
+    def test_find_winning_move_one_ahead(self):
         moves = [0,0,1,1,2,2]
         for move in moves:
             self.board.move(move)
         _, move, _ = self.solver.eval_moves(self.board, depth=1)
         self.assertEqual(move, 3)
+
+    def test_find_defending_moves_four_ahead(self):
+        moves = [3,3,4]
+        for move in moves:
+            self.board.move(move)
+        scores, _, _ = self.solver.eval_moves(self.board, depth=4)
+        self.assertEqual(scores[5], 0)
+        self.assertEqual(scores[2], 0)
+        for x in range(self.board.w):
+            if x == 2 or x == 5:
+                continue
+            self.assertLess(scores[x], 0)
